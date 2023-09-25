e5a18b031ac813cb6a97c1d636b87b8819734894
Xero kun <elo48858@gmail.com>
2023-09-26 00:21:23 +0100

    chore: Updated printed title text

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ mod guess;
 use guess::Guess;
 
 fn main() {
-    println!("Guessing Game");
+    println!("Guessing Game: Improved");
 
     let user_input = input::get_input(&"Your guess")
         .expect("Failed to read user guess");
-    
+
     let user_guess = Guess::from_string(&user_input)
         .expect("Guess should be a positive number");
 

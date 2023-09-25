b92c92b07ed4b4fd4748dcb78533c5dbbee3635b
Xero kun <elo48858@gmail.com>
2023-09-25 23:11:30 +0100

    test: Tested guess and input modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod input;
 mod guess;
 
+use guess::Guess;
+
 fn main() {
     println!("Guessing Game");
+    let user_input = input::get_input(&"Your guess")
+        .expect("Failed to read user guess");
+    let user_guess = Guess::from_string(&user_input)
+        .expect("Guess should be a positive number");
+
+    println!("{:?}", user_guess.value());
 }
